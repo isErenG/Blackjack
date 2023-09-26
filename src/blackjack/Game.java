965be958a51d95c98d8ed0a7ee184712fff475f4
ereng <erengkorkmaz@gmail.com>
2023-09-26 14:42:01 +0200

    added comments and rearranged some mechanics

--- src/blackjack/Game.java
+++ src/blackjack/Game.java
@@ -4,7 +4,6 @@ import blackjack.players.Dealer;
 import blackjack.players.Player;
 import blackjack.game_mechanics.GameMechanics;
 
-
 import java.util.concurrent.TimeUnit;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +24,7 @@ public class Game {
     List<Integer> dealerHand = new ArrayList<>();
     List<Integer> playerHand = new ArrayList<>();
 
-    private String getNextMove() {
-        System.out.println("Do you wish to hit or stand?");
-        return scanner.nextLine().toLowerCase();
-    }
-
-    private String quitGame() {
-        return scanner.nextLine().toLowerCase();
-    }
-
+    // Starts the game
     public void startGame() throws InterruptedException {
         // Initialize the dealer and player
         Dealer dealer = new Dealer();
@@ -46,16 +37,19 @@ public class Game {
             boolean wantsHit = true;
             boolean shouldDealerHit = true;
 
-            // Call the dealCards method from the dealer which returns the player and dealer hands
+            // Deals the cards to the player and dealer
             List<List<Integer>> hands = dealer.dealCards(deck, dealerHand, playerHand, dealerDeck);
             List<Integer> dealerHand = hands.get(0);
             List<Integer> playerHand = hands.get(1);
 
+            // Prompts the player to place a bet
             Integer bet = player.wager(scanner);
             scanner.nextLine();
 
+            // Displays the player's and dealer's hands
             System.out.println("\nThe dealer has a " + dealerHand.get(0) + "\nWhile you have a " + playerHand);
 
+            // While the player wants to hit and hasn't busted, continue hitting
             while (wantsHit) {
                 // Calculate the player's hand value
                 int playerHandValue = dealerDeck.updateHands(playerHand);
@@ -65,7 +59,7 @@ public class Game {
                     System.out.println("The player has busted.");
                     shouldDealerHit = false;
                     wantsHit = false;
-                } else if (getNextMove().equalsIgnoreCase("hit")) {
+                } else if (gameMechanics.getNextMove(scanner).equalsIgnoreCase("hit")) {
                     // Player wants to hit
                     System.out.println("\n" + player.hit(playerHand, deck, dealerDeck));
                 } else {
@@ -75,28 +69,32 @@ public class Game {
             }
 
 
+            // Reveal the dealer's second card
             System.out.println("\nThe dealer reveals a " + dealerHand.get(1) + "\n");
+
+            // While the dealer needs to hit, continue hitting
             while (shouldDealerHit) {
                 TimeUnit.SECONDS.sleep(2);
                 if (dealerDeck.updateHands(dealerHand) >= 17) {
+                    // Check the winner and display the result
                     System.out.println(gameMechanics.checkWinner(dealerDeck, dealerHand, playerHand, dealer, bet));
                     shouldDealerHit = false;
 
                 } else {
+                    // The dealer needs to hit
                     System.out.println("The dealer's hand is: " + dealer.hit(dealerHand, deck, dealerDeck));
                 }
             }
 
+            // Ask the player if they want to continue playing
             System.out.println("\nDo you want to continue playing?\n[Options] Press enter or type no");
-            if (quitGame().equalsIgnoreCase("No")) {
+            if (gameMechanics.quitGame(scanner).equalsIgnoreCase("No")) {
                 continuePlaying = false;
             } else {
+                // Clear the player and dealer hands
                 playerHand.clear();
                 dealerHand.clear();
             }
         }
     }
-
-
-
 }
--- src/blackjack/game_mechanics/GameMechanics.java
+++ src/blackjack/game_mechanics/GameMechanics.java
@@ -4,31 +4,44 @@ import blackjack.Deck;
 import blackjack.players.Dealer;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class GameMechanics {
+
+    // Asks the player for their next move (hit or stand)
+    public String getNextMove(Scanner scanner) {
+        System.out.println("Do you wish to hit or stand?");
+        return scanner.nextLine().toLowerCase();
+    }
+
+    // Asks the player to confirm if they want to quit the game
+    public String quitGame(Scanner scanner) {
+        return scanner.nextLine().toLowerCase();
+    }
+
     public String checkWinner(Deck dealerDeck, List<Integer> dealerHand, List<Integer> playerHand, Dealer dealer, Integer bet) {
         Integer dealerValue = dealerDeck.updateHands(dealerHand);
         Integer playerValue = dealerDeck.updateHands(playerHand);
 
         if (playerValue > dealerValue && playerValue < 21) {
-            return "The Player wins!\n$" + dealer.payOut(bet, "player");
+            return "\nThe Player wins!\n$" + dealer.payOut(bet, "player");
 
         } else if (playerValue == 21 && playerHand.size() == 2) {
-            return "Blackjack! The Player wins!\n$" + dealer.payOut(bet, "player");
+            return "\nBlackjack! The Player wins!\n$" + dealer.payOut(bet, "player");
 
         } else if (dealerValue > 21) {
-            return "Dealer busts! Player wins!\n$" + dealer.payOut(bet, "player");
+            return "\nDealer busts! Player wins!\n$" + dealer.payOut(bet, "player");
 
         } else if (playerValue > 21) {
             dealer.payOut(bet, "dealer");
-            return "The Player busts! The Dealer wins!\n$" + dealer.payOut(bet, "dealer");
+            return "\nThe Player busts! The Dealer wins!\n$" + dealer.payOut(bet, "dealer");
 
         } else {
             if (dealerValue.equals(playerValue)) {
-                return "Push!\n$" + dealer.payOut(bet, "stalemate");
+                return "\nPush!\n$" + dealer.payOut(bet, "stalemate");
 
             } else {
-                return "The Dealer wins!\n$" + dealer.payOut(bet, "dealer");
+                return "\nThe Dealer wins!\nPayout:$" + dealer.payOut(bet, "dealer");
             }
         }
     }
